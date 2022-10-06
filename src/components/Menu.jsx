d67c0f52e1408ededdc954b686591baa63baaa29
Daniel Campelo <danielcampelo159@gmail.com>
2022-10-06 15:21:10 -0300

    Finishing menu container and nav links

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 
-// import Nav from "./Nav";
+import Nav from "../components/Nav";
+import AvatarContainer from "../components/AvatarContainer";
+import '../CSS/Home.css';
 
-export default function Menu() {
+export default function Home() {
     return (
-        <div>New Component</div>
-    //   <div>
-    //     <Nav />
-    //   </div>
+      <div className="menu-container">
+        <AvatarContainer />
+        <Nav />
+        <ul className="socials">
+          <li><a rel="GitHub" href="https://www.linkedin.com/in/daniel-campelo-338710230/"><ion-icon name="logo-github" className="social-icon"></ion-icon></a></li>
+          <li><a rel="Linkedin" href="https://github.com/danielcampl"><ion-icon name="logo-linkedin" className="social-icon"></ion-icon></a></li>
+          <li><a rel="Mail" href="mailto://danielcampelo159@gmail.com"><ion-icon name="mail" className="social-icon"></ion-icon></a></li>
+        </ul>
+      </div>
     );
 }
