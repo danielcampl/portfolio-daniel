f5e5e67dc21458790e535ac00045a6d2ac7cf55a
Daniel Campelo <danielcampelo159@gmail.com>
2022-10-07 12:00:20 -0300

    Fix z-index from background

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,11 +1,40 @@
 import React from "react";
 
-import '../CSS/ContactForm.css';
+import "../CSS/ContactForm.css";
 
 export default function ContactForm() {
-    return (
-      <div className="contact-container">
+  return (
+    <section className="contact-container">
+      <div>
         <h1>Contact me</h1>
       </div>
-    );
+      <div className="row100">
+        <div className="col">
+          <div className="input-box">
+            <input type="text" name="" required="required" />
+            <span className="text">First Name</span>
+            <span className="line" />
+          </div>
+        </div>
+        <div className="col">
+          <div className="input-box">
+            <input type="text" name="" required="required" />
+            <span className="text">Last Name</span>
+            <span className="line" />
+          </div>
+        </div>
+      </div>
+
+      <div className="row100">
+        <div className="col">
+          <div className="input-box textarea">
+            <textarea name="" id="" cols="30" rows="10" required="required" />
+            <span className="text">Type your message</span>
+            <span className="line" />
+          </div>
+        </div>
+      </div>
+
+    </section>
+  );
 }
