c92d426266f90befaec1544d10f9a562a30176bc
Daniel Campelo <danielcampelo159@gmail.com>
2022-10-08 17:30:46 -0300

    Redirect message form for email contact + "message succesfully sent" message

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 import "../CSS/ContactForm.css";
 
+const Result = () => {
+  return <p className="row50">Your message has been succesfully sent 🚀</p>;
+};
+
 export default function ContactForm() {
+  const [result, showResult] = useState(false);
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_1luzg18",
+        "template_2dbflqg",
+        form.current,
+        "bSdbJUtA_u6ASV4ym"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+    showResult(true);
+  };
+
   return (
-    <section>
+    <form ref={form} onSubmit={sendEmail}>
       <div className="contact-container">
         <h1>Contact me</h1>
         <p>
@@ -16,14 +46,14 @@ export default function ContactForm() {
           <div className="row100">
             <div className="col">
               <div className="input-box-name">
-                <input type="text" name="" required="required" />
+                <input type="text" name="firstname" required="required" />
                 <span className="text">First Name</span>
                 <span className="line" />
               </div>
             </div>
             <div className="col">
               <div className="input-box-name">
-                <input type="text" name="" required="required" />
+                <input type="text" name="lastname" required="required" />
                 <span className="text">Last Name</span>
                 <span className="line" />
               </div>
@@ -33,7 +63,7 @@ export default function ContactForm() {
           <div className="row100">
             <div className="col">
               <div className="input-box">
-                <input type="text" name="" required="required" />
+                <input type="text" name="email" required="required" />
                 <span className="text">Email</span>
                 <span className="line" />
               </div>
@@ -44,7 +74,7 @@ export default function ContactForm() {
         <div className="row100">
           <div className="col">
             <div className="input-box-message textarea">
-              <textarea required="required" />
+              <textarea required="required" name="message" />
               <span className="text">Type your message</span>
               <span className="line" />
             </div>
@@ -53,10 +83,13 @@ export default function ContactForm() {
         {/* SEND BUTTOM */}
         <div className="row100 send-bnt">
           <div className="col">
-            <input type="submit" value="Send message!" />
+            <button type="submit" value="Send message!">
+              Send message!
+            </button>
           </div>
         </div>
+        <div className="row">{result ? <Result /> : null}</div>
       </div>
-    </section>
+    </form>
   );
 }
