69c0df473fb3a2ec583de5cfafb14d46c3d9fcf3
Daniel Campelo <danielcampelo159@gmail.com>
2022-10-09 17:41:42 -0300

    Fix english

--- src/pages/About.js
+++ src/pages/About.js
@@ -27,9 +27,9 @@ export default function About() {
             Me, Myself & I
           </div>
           <div className="about-text">
-            <p>I have knowledge in front-end and back-end web development, enjoy working in teams and keep lacking to learn more, not only in web development, but also in other areas encompassed by Software Engineering. In this way, acquire more experience.</p>
+            <p>I have knowledge in front-end and back-end, enjoy working in teams and keep lacking to learn more, not only in web development, but also in other areas encompassed by Software Engineering. In this way, acquire more experience.</p>
             <p>Well-organised person, problem solver with high attention to detail. Fan of Games online, outdoor activities and TV series. A family person and lead by good friendships.</p>
-            <p>Undergraduate course: Trybe | 3º Período (03/2022 - 03/2023)</p>
+            <p>Undergraduate course: Trybe | 3º Module (03/2022 - 03/2023)</p>
             <Link to="/contact"><p className="linkto">Let's make something special.</p></Link>
           </div>
         </section>
