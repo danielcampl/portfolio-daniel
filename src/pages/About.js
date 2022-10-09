77a69f429453246f4ab89f26a6609daf4f221b82
Daniel Campelo <danielcampelo159@gmail.com>
2022-10-08 22:53:56 -0300

    Add content at Home page and change background

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,10 @@
 import React from "react";
 
-import Background from "../components/Background";
 import Menu from "../components/Menu";
 
 export default function About() {
   return (
     <div>
-      <Background />
       <Menu />
     </div>
   );
