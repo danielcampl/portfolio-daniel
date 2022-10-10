0e6540cb1b35fd24f7acfd4447ef10dd63f37780
Daniel Campelo <danielcampelo159@gmail.com>
2022-10-10 20:03:34 -0300

    Add animate to pages

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 import Menu from "../components/Menu";
 import ContactForm from "../components/ContactForm";
@@ -7,7 +8,13 @@ export default function Contact() {
   return (
     <div>
       <Menu />
-      <ContactForm />
+      <motion.div
+        animate={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 0.4 }}
+      >
+        <ContactForm />
+      </motion.div>
     </div>
   );
 }
