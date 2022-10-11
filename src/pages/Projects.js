673192cde1db545e5db047c08dd2c20657491ceb
Daniel Campelo <danielcampelo159@gmail.com>
2022-10-10 21:27:16 -0300

    Add project magicwall box

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import { motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 
 import Menu from '../components/Menu';
 
@@ -7,6 +7,20 @@ export default function Projects() {
     return (
       <div>
         <Menu />
+        <motion.div 
+        animate={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 0.4 }}
+        className="magicwall wall"
+      >
+          <ul>
+            <li>
+              <div className="magicwall-item lazyload">
+                {/* <img src={} alt="project-image" /> */}
+              </div>
+            </li>
+          </ul>
+        </motion.div>
       </div>
     );
 }
